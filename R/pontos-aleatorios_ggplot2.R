3fe450fb19c3186cb183c70545cde8f2d8b4e082
Ícaro Vidal Freire <icarofreire7@gmail.com>
2021-03-12 00:36:45 -0300

    script para produção dos gráficos

--- R/pontos-aleatorios_ggplot2.R
+++ R/pontos-aleatorios_ggplot2.R
@@ -1,1 +1,32 @@
-# fazer com 100, 1000 e 5000 pontos
+#==============================================================================
+# Carregando os pacotes
+#------------------------------------------------------------------------------
+library(tidyverse) # pacote para análise de dados
+library(ggforce)   # pacote usado para criar um círculo unitário
+#==============================================================================
+
+# Função para gerar o gráfico
+
+grafico_pontos <- function(n){
+  dt <- tibble(
+    x = runif(n, -1, 1),
+    y = runif(n, -1, 1)
+  )
+  
+  grafico <- dt %>% 
+    ggplot(aes(x, y)) +
+    geom_circle(aes(x0 = 0, y0 = 0, r = 1), color = "red", inherit.aes = FALSE) +
+    geom_point(alpha = 0.5, color = "blue")
+  
+  grafico
+}
+
+# Testando com os ponto 100, 1000 e 5000
+
+grafico_pontos(100)
+
+grafico_pontos(1000)
+
+grafico_pontos(5000)
+
+# Obs.: Quanto mais pontos você coloca, mais tempo vai precisar para produção.
